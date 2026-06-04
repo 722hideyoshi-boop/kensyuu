6e9a6a7e804f9155176c785b06627922dbc642d1
722hi <722hi@LAPTOP-NC5JGPSM>
2026-06-04 18:44:27 +0900

    6/4 18:00

---
+++ ch6/src/ch6/Main6_6_1_calclogic.java
@@ -0,0 +1,12 @@
+package ch6;
+
+public class Main6_6_1_calclogic {
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+
+	public static int hiku(int a, int b) {
+		return (a - b);
+	}
+
+}
